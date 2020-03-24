b50cd73e02f0c016f3dcefde482f967649bb35c7
WickedTree <50127946+WickedTree@users.noreply.github.com>
2020-03-24 16:42:00 -0700

    Update covid19.py

--- hangupsbot/plugins/covid19.py
+++ hangupsbot/plugins/covid19.py
@@ -1,6 +1,4 @@
-import logging
-import plugins
-import requests
+import logging, plugins, requests, json
 
 logger = logging.getLogger(__name__)
 
@@ -13,7 +11,7 @@ def covid19(bot, event, *args):
         pagearray = json.loads(page.text)
         cases = str(pagearray["cases"])
         deaths = str(pagearray["deaths"])
-        html_text = "<strong>The COVID-19 outbreak internationaly has " + cases + " cases and " + deaths + " deaths</strong><br />Original Plugin by KD7T"
+        html_text = "<strong>The COVID-19 outbreak internationally has " + cases + " cases and " + deaths + " deaths</strong><br />Original Plugin by KD7T"
     except:
         html_text = "Unable to get covid-19 data right now"
         logger.exception(html_text)
