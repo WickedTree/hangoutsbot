6e80f41d498a6c0922bd08ac186d6ea5689158bc
WickedTree <50127946+WickedTree@users.noreply.github.com>
2020-03-24 16:32:06 -0700

    Create covid19.py

---
+++ hangupsbot/plugins/covid19.py
@@ -0,0 +1,20 @@
+import logging
+import plugins
+import requests
+
+logger = logging.getLogger(__name__)
+
+def _initialise(bot):
+    plugins.register_user_command(["covid19"])
+    
+def covid19(bot, event, *args):
+    try:   
+        page = requests.get('https://coronavirus-19-api.herokuapp.com/all')
+        pagearray = json.loads(page.text)
+        cases = str(pagearray["cases"])
+        deaths = str(pagearray["deaths"])
+        html_text = "<strong>The COVID-19 outbreak internationaly has " + cases + " cases and " + deaths + " deaths</strong>"
+    except:
+        html_text = "Unable to get covid-19 data right now"
+        logger.exception(html_text)
+    yield from bot.coro_send_message(event.conv_id, html_text)
